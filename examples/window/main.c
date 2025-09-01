0a358ca6e2ff67abb445c794d85e81ecf22ea3cd
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-01 17:40:57 -0600

    ok yay

--- examples/window/main.c
+++ examples/window/main.c
@@ -3,10 +3,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void cb_size(GLFWwindow* window, int width, int height) {
+    glViewport(0,0, width, height);
+}
+
 int main(void) {
     if (!glfwInit()) { printf("goodbye world, never knew you!\n"); return 1; }
 
-    glfwWindowHint(int hint, int value)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800,600, "basic window example", 0,0);
+    if (!window) { printf("failed to create window!\n"); return 1; }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, cb_size);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { printf("failed to load opengl!\n"); return 1; }
+
+    glViewport(0,0,800,600);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(.2f, .3f, .4f, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
 
     glfwTerminate();
 
