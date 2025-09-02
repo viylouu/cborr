32990dfd10a17f2c49e47a58bff7f596e25816af
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-01 19:24:33 -0600

    clean

--- eng/draw/draw.h
+++ eng/draw/draw.h
@@ -1,14 +1,17 @@
 #ifndef DRAW_H
 #define DRAW_H
 
+// ------- funcs
+void IMPL_cbClear(float r, float g, float b, float a);
+
+// ------- function overload helpers
 #define VA_NARGS_IMPL(_1,_2,_3,_4,N,...) N
 #define VA_NARGS(...) VA_NARGS_IMPL(__VA_ARGS__,4,3,2,1,0)
 
 #define CAT2(a,b) a##b
 #define CAT(a,b) CAT2(a,b)
 
-void IMPL_cbClear(float r, float g, float b, float a);
-
+// ------- function overloads
 #define SELECT_FCLEAR(...) CAT(fclear_, VA_NARGS(__VA_ARGS__))
 #define cbFClear(...) SELECT_FCLEAR(__VA_ARGS__)(__VA_ARGS__)
 #define fclear_1(r) IMPL_cbClear(r,r,r,1.f)
