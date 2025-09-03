d266da27cbb8283a1b6657bf001b65ced73a00e0
viylouu_the_second <29hunter.shucart@boiseschools.net>
2025-09-03 13:51:10 -0600

    wee

--- eng/phys/phys.c
+++ eng/phys/phys.c
@@ -1,9 +1,10 @@
 #include "phys.h"
+
 #include <stdbool.h>
 #include <math.h>
 
 const float GRAV = 9.81;
-//maybe change return type idk tho
+
 bool cbCheckRectColl(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2){
     return (x1 + w1 >= x2 && x1 <= x2 + w2 &&
             y1 <= y2 + h2 && y1 + h1 >= y2);
