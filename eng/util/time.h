9fd7a8e05a807b6fb0f1dc4956d126d062e8d0a9
viylouu_the_second <29hunter.shucart@boiseschools.net>
2025-09-11 13:10:01 -0600

    add / destroy

--- eng/util/time.h
+++ eng/util/time.h
@@ -1,4 +1,13 @@
 #ifndef TIME_H
 #define TIME_H
 
+typedef struct {
+    float speed;
+    float time;
+    float delta;
+} CBtime;
+
+CBtime* cbMakeTimer(float offset);
+void cbDestroyTimer(CBtime* timer);
+
 #endif
