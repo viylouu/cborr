3105b8319f789d39ab6c6b890a40c16cc70c079d
zop3 <jackshaffley@gmail.com>
2025-09-12 18:25:24 -0600

    goog

---
+++ eng/audio/audio.h
@@ -0,0 +1,14 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+typedef struct {
+    char filePath[256];
+    char fileName[256]; //name
+    float vol;
+    int loop;  
+    int musicFile;  //specifies if its a music file or not
+} cBAudioFile;
+
+int audio_init();
+
+#endif
